7d93766ecbbb315ed182756960fd9ae8d9999d7b
Rayaan <rayannzargar@gmail.com>
2026-04-09 20:58:04 +0530

    Brand Name generator

---
+++ project 1 brand name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator!")
+a=input("First three letters of your name are:\n ")
+b=input("last three letters of the month you were born in:\n ")
+print("Your Brand Name is: " + (a+b))
